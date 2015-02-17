9b1e5cf839215f9d56b125a5aa381c02d229d29e
Rob Graziano <robgraziano@earthlink.net>
2015-02-16 21:25:05 -0500

    retrieve poster url

--- models.py
+++ models.py
@@ -1,8 +1,18 @@
 """The movie class"""
 
+from imdb import IMDb
+from BeautifulSoup import BeautifulSoup
+
 class Movie:
-	poster_image_url = ''
-	trailer_youtube_url = ''
 
 	def __init__(self, title):
 		self.title = title
+		self.poster_image_url = self.get_poster(self.title)
+		self.trailer_youtube_url = ''
+
+	def get_poster(self, title):
+		db_access = IMDb()
+		first_result = db_access.search_movie(title)[0]
+		movie_id = first_result.movieID
+		movie = db_access.get_movie(movie_id)
+		return movie['cover url']
