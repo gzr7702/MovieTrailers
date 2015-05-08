76b9e8aa01f898d81e84c3d6490015ff0da05040
Rob Graziano <robgraziano@earthlink.net>
2015-05-08 11:02:41 -0400

    refactor w/json

--- movies.py
+++ movies.py
@@ -4,42 +4,35 @@
 """
 
 import fresh_tomatoes
-from imdb import IMDb
-from BeautifulSoup import BeautifulSoup
+import json
 
 class Movie:
-	"""The trailer url and title are passed in to the constructor,
-		the rest of the info pulled from the IMDb database."""
-	def __init__(self, title, trailer_url):
+	"""The title is passed in to the constructor,
+		the rest of the info pulled from the local database."""
+	def __init__(self, title):
 		self.title = title
-		self.trailer_youtube_url = trailer_url
+		with open('data2.json') as f:
+			self.raw_movie_info = json.load(f)
 		self.get_movie_info()
 
 	def get_movie_info(self):
-		db_access = IMDb()
-		first_result = db_access.search_movie(self.title)[0]
-		movie_id = first_result.movieID
-		movie = db_access.get_movie(movie_id)
-		self.stars = str(movie['actors'][0]) + '/' + str(movie['actors'][1]) + '/' + str(movie['actors'][2])
-		self.poster_image_url = movie['cover url']
-		self.plot_outline = movie['plot outline']
+		"""We load the data from data.json file in the constructor. If we 
+			were using a realdatabase, we would have queries here.
+		"""
+		self.trailer_youtube_url = self.raw_movie_info[self.title]['trailer']
+		self.stars = '/'.join(self.raw_movie_info[self.title]['stars'])
+		self.poster_image_url =  self.raw_movie_info[self.title]['poster_url']
+		self.plot_outline =  self.raw_movie_info[self.title]['blurb']
 
 def movie_trailer():
 	""" Craetes the list of movie objects, then calls the main module
 	"""
-	#titles = ["Goodfellas", "Godfather", "Big Lebowski"]
-	titles = {"Goodfellas":'https://www.youtube.com/watch?v=qo5jJpHtI1Y', 
-			"The Godfather": 'https://www.youtube.com/watch?v=sY1S34973zA', 
-			"Big Lebowski": 'https://www.youtube.com/watch?v=cd-go0oBF4Y',
-			"Casablanca": 'https://www.youtube.com/watch?v=EJvlGh_FgcI',
-			"State of Grace": 'https://www.youtube.com/watch?v=PN_L96iEQfQ',
-			"The Girl with the Dragon Tattoo": 'https://www.youtube.com/watch?v=DqQe3OrsMKI'
-			}
-
+	titles = ["Goodfellas", "The Godfather", "Big Lebowski", "Casablanca",\
+			"State of Grace", "The Girl with the Dragon Tattoo"]
 	movies = []
 
-	for title, trailer in titles.items():
-		movies.append(Movie(title, trailer))
+	for title in titles:
+		movies.append(Movie(title))
 
 	fresh_tomatoes.open_movies_page(movies)
 
