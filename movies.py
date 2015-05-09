848008dda00b09fced6266cd1359a6c3ad00b7c4
Rob Graziano <robgraziano@earthlink.net>
2015-05-09 13:08:59 -0400

    pass data.json as argument

--- movies.py
+++ movies.py
@@ -5,37 +5,45 @@
 
 import fresh_tomatoes
 import json
+import sys
+import os
 
 class Movie:
-	"""The title is passed in to the constructor,
+	""" The title is passed in to the constructor,
 		the rest of the info pulled from the local database."""
-	def __init__(self, title):
+	def __init__(self, title, movie_info):
 		self.title = title
-		with open('data.json') as f:
-			self.raw_movie_info = json.load(f)
-		self.get_movie_info()
+		self.get_movie_info(movie_info)
 
-	def get_movie_info(self):
-		"""We load the data from data.json file in the constructor. If we 
-			were using a realdatabase, we would have queries here.
+	def get_movie_info(self, minfo):
+		""" We load the data from data.json file in the constructor. If we 
+			were using a real database, we would have queries here.
 		"""
-		self.trailer_youtube_url = self.raw_movie_info[self.title]['trailer']
-		self.stars = '/'.join(self.raw_movie_info[self.title]['stars'])
-		self.poster_image_url =  self.raw_movie_info[self.title]['poster_url']
-		self.plot_outline =  self.raw_movie_info[self.title]['blurb']
+		#import pdb; pdb.set_trace()
+		self.trailer_youtube_url = minfo['trailer']
+		self.stars = '/'.join(minfo['stars'])
+		self.poster_image_url =  minfo['poster_url']
+		self.plot_outline =  minfo['blurb']
 
-def movie_trailer():
-	""" Craetes the list of movie objects, then calls the main module
+def movie_trailer(args):
+	""" Creates the list of movie objects, then calls the main module
 	"""
-	titles = ["Goodfellas", "The Godfather", "Big Lebowski", "Casablanca",\
-			"State of Grace", "The Girl with the Dragon Tattoo"]
+
+	json_file = args
+	if not os.path.exists(json_file):
+		print("You didn't supply a valid file")
+		sys.exit(1)
+
+	with open(json_file) as f:
+		movie_info = json.load(f)
+
 	movies = []
 
-	for title in titles:
-		movies.append(Movie(title))
+	for title in movie_info:
+		movies.append(Movie(title, movie_info[title]))
 
 	fresh_tomatoes.open_movies_page(movies)
 
 
 if __name__ == '__main__':
-	movie_trailer()
+	movie_trailer(sys.argv[1])
