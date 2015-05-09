8c312fa6ca12863df8018e50ec858e03f3cd6522
Rob Graziano <robgraziano@earthlink.net>
2015-05-09 18:54:56 -0400

    added comments

--- movies.py
+++ movies.py
@@ -12,6 +12,7 @@ class Movie:
 	def __init__(self, title, movie_info):
 		""" The title is passed in to the constructor,
 			the rest of the info is passed in as json."""
+
 		self.title = title
 		self.get_movie_info(movie_info)
 
@@ -28,18 +29,24 @@ def movie_trailer(args):
 	""" Creates the list of movie objects, then calls the main module """
 
 	json_file = args
+	# Check if file exists, if not, exit
 	if not os.path.exists(json_file):
 		print("You didn't supply a valid file")
 		sys.exit(1)
 
+	# Open file and read contents into a variable
 	with open(json_file) as f:
 		movie_info = json.load(f)
 
+	# List to hold movie objects
 	movies = []
 
+	# Cycle thorugh each object in the json file
+	# and use it to create a movie object
 	for title in movie_info:
 		movies.append(Movie(title, movie_info[title]))
 
+	# Ok. We have our movies, let's create the page
 	fresh_tomatoes.open_movies_page(movies)
 
 
